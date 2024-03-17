3a0eacdb3eb768e8e1dc6105f9288929b1ff8bea
Andrea Fabris <afabris86@gmail.com>
2024-03-17 14:16:11 +0100

    feat(Tailwindcss): add tailwind css

--- src/AddItem.js
+++ src/AddItem.js
@@ -4,21 +4,32 @@ const AddItem = ({ onAddItem }) => {
   const [description, setDescription] = useState();
 
   const handleOnButtonClick = () => {
-    onAddItem(description);
-    setDescription("");
+    if (description) {
+      onAddItem(description);
+      setDescription("");
+    }
   };
 
   return (
-    <div>
+    <form
+      className="flex w-auto mt-8 p-2 border-2 border-[#262626] focus-within:border-[#919191] my-4 rounded-lg"
+      onSubmit={(e) => e.preventDefault()}
+    >
+      <button
+        className="bg-[#e68c3a] hover:bg-[#dc6900] text-white font-medium py-2 px-4 rounded-full text-base"
+        onClick={handleOnButtonClick}
+      >
+        +
+      </button>
       <input
         placeholder="What do you need to do?"
         value={description}
         onInput={(e) => {
           setDescription(e.target.value);
         }}
+        className="border-none text-white focus:outline-none overflow-hidden bg-transparent px-4 "
       />
-      <button onClick={handleOnButtonClick}>ADD</button>
-    </div>
+    </form>
   );
 };
 
--- src/TodoItems.js
+++ src/TodoItems.js
@@ -8,18 +8,23 @@ const TodoItems = ({ items, onItemClicked }) => {
     <>
       {items.map((item) => {
         return (
-          <>
+          <div className="my-2 py-4 flex gap-2 px-2 bg-[#262626] hover:bg-[#292525] rounded-lg">
             <input
               type="checkbox"
               onClick={(e) => {
                 handleOnItemClicked(item, e.target.checked);
               }}
+              className="w-5 h-5 border-2 border-[#919191] rounded-sm bg-white accent-[#e68c3a]"
             />
-            <label>
-              {item.isDone ? <s>{item.description}</s> : item.description}
+            <label className="text-base font-normal text-[#dbdbdb]">
+              {item.isDone ? (
+                <s className="decoration-2">{item.description}</s>
+              ) : (
+                item.description
+              )}
             </label>
             <br />
-          </>
+          </div>
         );
       })}
     </>
